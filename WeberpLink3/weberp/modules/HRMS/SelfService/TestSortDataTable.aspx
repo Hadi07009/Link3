19f448d421be1ac8c7493b7cea9357eaf28de04a
HADI <hadi.mbstu@gmail.com>
2016-07-25 18:32:46 +0600

    test sort data table in c#

---
+++ WeberpLink3/weberp/modules/HRMS/SelfService/TestSortDataTable.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="TestSortDataTable.aspx.cs" Inherits="modules_HRMS_SelfService_TestSortDataTable" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 87px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table style="width: 99%;text-align:left">
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">
+                    <asp:Label ID="Label1" runat="server" Text="Employee code"></asp:Label>
+                </td>
+                <td>:</td>
+                <td>
+                    <asp:TextBox ID="txtEmployeeCode" runat="server" Width="350px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">
+                    <asp:Label ID="Label2" runat="server" Text="Employee Name"></asp:Label>
+                </td>
+                <td>:</td>
+                <td>
+                    <asp:TextBox ID="txtEmployeeName" runat="server" Width="350px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>
+                    <asp:Button ID="btnSave" runat="server" OnClick="btnSave_Click" Text="Save" Width="100px" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <asp:GridView ID="grdEmployeeRecord" runat="server" Width="100%">
+                    </asp:GridView>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WeberpLink3/weberp/modules/HRMS/SelfService/TestSortDataTable.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+public partial class modules_HRMS_SelfService_TestSortDataTable : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            grdEmployeeRecord.DataSource = ViewState["employeeRecord"];
+            grdEmployeeRecord.DataBind();
+        }
+    }
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            GetDataIntoGrid();
+
+        }
+        catch (Exception msgException)
+        {
+
+            MessageBox.Show(msgException.Message);
+        }
+    }
+
+    private void GetDataIntoGrid()
+    {
+        try
+        {
+            var employeeCode = txtEmployeeCode.Text == string.Empty ? null : txtEmployeeCode.Text;
+            var employeeName = txtEmployeeName.Text == string.Empty ? null : txtEmployeeName.Text;
+
+            int slNo = 1;
+            
+            var dt = new DataTable();
+            DataRow dr;
+            dt.Columns.Add(new DataColumn("slNo", typeof(int)));
+            dt.Columns.Add(new DataColumn("txtEmployeeCode", typeof(String)));
+            dt.Columns.Add(new DataColumn("txtEmployeeName", typeof(String)));
+
+            if (ViewState["employeeRecord"] != null)
+            {
+                var dtTable = (DataTable)ViewState["employeeRecord"];
+
+                foreach (DataRow drSl in dtTable.Rows)
+                {
+                    int accountLevel = drSl.Field<int>("slNo");
+                    slNo = Math.Max(slNo, accountLevel) + 1;
+                }
+
+                var count = dtTable.Rows.Count;
+                for (var i = 0; i < count + 1; i++)
+                {
+                    dt = (DataTable)ViewState["employeeRecord"];
+                    if (dt.Rows.Count <= 0) continue;
+                    dr = dt.NewRow();
+                    dr[0] = dt.Rows[0][0].ToString();
+                }
+                dr = dt.NewRow();
+                dr[0] = slNo;
+                dr[1] = employeeCode;
+                dr[2] = employeeName;
+
+                dt.Rows.Add(dr);
+
+            }
+            else
+            {
+                dr = dt.NewRow();
+                dr[0] = slNo;
+                dr[1] = employeeCode;
+                dr[2] = employeeName;
+
+                dt.Rows.Add(dr);
+            }
+
+            dt.DefaultView.Sort = "slNo DESC";
+            grdEmployeeRecord.DataSource = dt;
+            grdEmployeeRecord.DataBind();
+            ViewState["employeeRecord"] = dt;
+
+        }
+        catch (Exception msgException)
+        {
+
+            throw msgException;
+        }
+    }
+}
